e66336c33ed447263fc1f87225b8ff002b4ecd35
Jeff Horn <jeff.horn@reachtech.com>
2019-01-28 13:06:06 -0800

    board/reach/common/imx: Move /custom to separate filesystem
    * This adds the /custom Qml app to separate application partition which
    gets mounted in boot. This is needed so we can update the root partition
    and not affect the customer application.
    
    Signed-off-by: Jeff Horn <jeff.horn@reachtech.com>

--- board/reach/common/imx/post-image.sh
+++ board/reach/common/imx/post-image.sh
@@ -3,8 +3,18 @@
 BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+APPIMAGE_CFG="${BOARD_DIR}/appimage.cfg"
+APPIMAGE_TMP="${BUILD_DIR}/appimage.tmp"
 
 rm -rf "${GENIMAGE_TMP}"
+rm -rf "${APPIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${BOARD_DIR}/appfs"     \
+	--tmppath "${APPIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${APPIMAGE_CFG}"
 
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
