377d6f15f83564378e8253861c5c28cfb247eca6
Jeff Horn <jeff.horn@reachtech.com>
2019-02-27 13:00:59 -0800

    reach_imx6dl_7_p_qt5_defconfig: Add fblogo package
    * fblogo package is a simple framebuffer splash utility.
    
    Signed-off-by: Jeff Horn <jeff.horn@reachtech.com>

---
+++ package/fblogo/fblogo.mk
@@ -0,0 +1,14 @@
+FBLOGO_VERSION = 2bb6674e8052b5478885e7c63af0f9fff4210763
+FBLOGO_LICENSE = GPL-3.0+
+FBLOGO_SITE = https://github.com/jmore-reachtech/reach-fblogo.git
+FBLOGO_SITE_METHOD = git
+FBLOGO_CONF_OPTS = --prefix=/usr
+FBLOGO_AUTORECONF = yes
+
+define FBLOGO_PRE_CONFIGURE_AUTOGEN
+	(cd $(@D); ./autogen.sh)
+endef
+
+FBLOGO_PRE_CONFIGURE_HOOKS += FBLOGO_PRE_CONFIGURE_AUTOGEN
+
+$(eval $(autotools-package))
